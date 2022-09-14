3076c01db4685f3eaa961fde25d5a8fe4f20d84e
dphiane <108275807+dphiane@users.noreply.github.com>
2022-09-14 15:05:13 +0200

    sauce images

--- back/controllers/sauce.js
+++ back/controllers/sauce.js
@@ -1,6 +1,6 @@
 const Sauce = require('../models/Sauce');
 const fs = require('fs');
-const sauce = require('../models/Sauce');
+
 
 exports.createSauce = (req, res, next) => {
     const sauceObject = JSON.parse(req.body.sauce);
@@ -9,12 +9,17 @@ exports.createSauce = (req, res, next) => {
     const sauce= new Sauce({
       ...sauceObject,
       userId: req.auth.userId,
-      imageUrl:`${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+      imageUrl:`${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+      likes: 0,
+      dislikes: 0,
+      usersLiked: [],
+      usersdisLiked: [],
     });
 sauce.save()
 .then(()=>{res.status(201).json({message:'Sauce enregistrée'})})
 .catch(error=> {res.status(400).json( {error})})
   };
+
 exports.getOneSauce = (req, res, next) => {
   Sauce.findOne({
     _id: req.params.id
@@ -34,7 +39,7 @@ exports.getOneSauce = (req, res, next) => {
 exports.modifySauce = (req, res, next) => {
   const sauceObject= req.file ? {
     ... JSON.parse(req.body.sauce),
-    imageUrl: `${req.protocol}://${req.get('host')}//images/${req.filename}`
+    imageUrl:`${req.protocol}://${req.get('host')}/images/${req.file.filename}`,  
   }: {...req.body};
   
   delete sauceObject.userId;
