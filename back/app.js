1fb2f043915e4a63cc3825a87b3b8f596326a907
fz <phiane.d@gmail.com>
2022-09-23 11:40:12 +0200

    helmet cors

--- back/app.js
+++ back/app.js
@@ -12,7 +12,7 @@ const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 
 const app = express();
-
+app.use(express.json());
 mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.x9mo6th.mongodb.net/?retryWrites=true&w=majority`,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
@@ -26,9 +26,12 @@ app.use((req, res, next) => {
     next();
   });
 
-  app.use(express.json());
   app.use(cors());
-  app.use(helmet());
+  app.use(
+    helmet({
+      crossOriginResourcePolicy: false,
+    })
+  );
   app.use(mongoSanitize());
   app.use(rateLimit.limiter)
   app.use('/api/auth', userRoutes);
--- back/routes/user.js
+++ back/routes/user.js
@@ -5,6 +5,6 @@ const password = require('../middleware/password');
 const rateLimit= require('../middleware/rate-limit');
 
 router.post('/signup', password, userCtrl.signup);
-router.post('/login',rateLimit.limiter, userCtrl.login);
+router.post('/login', rateLimit.limiter, userCtrl.login);
 
 module.exports = router;
